b2a9bd769c398f9122dbf257d3abbfaafd360c6d
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2025-11-21 11:05:06 -0800

    More string practicing via stories and logging strings to console. Basic easy stuff.

---
+++ html/javascript-projects/story-output/script.js
@@ -0,0 +1,20 @@
+let adjective, noun, verb, place, adjective2, noun2;
+adjective = "stinky";
+noun = "chonda";
+verb = "chorting";
+place = "Chocoland";
+adjective2 = "bad";
+noun2 = "4ski";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "smelly";
+noun = "choizee";
+verb = "jorkin";
+place = "somewhere";
+adjective2 = "swollen";
+noun2 = "jelly";
+console.log("");
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: "+ secondStory);
